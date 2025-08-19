36117cfe264a049fadf7fbfbfb1c9f9d0ee26924
Julian Martinez <47957027+julian1010011010@users.noreply.github.com>
2025-08-18 19:28:36 -0500

    Add files via upload

---
+++ juego/src/main.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser'
+import GameScene from './scenes/GameScene.js'
+
+const width = 480
+const height = 800
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#0f1020',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width,
+    height
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 900 },
+      debug: false
+    }
+  },
+  scene: [GameScene]
+}
+
+new Phaser.Game(config)
